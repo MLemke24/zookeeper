8f97285fa2db4a215b9c5d76f0dc22be4c2004f0
Matthew Lemke <matthewlemke@Matthews-MBP.lan>
2021-04-17 19:56:49 -0500

    mod one done

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Query Filter
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // filtered by diet
@@ -34,6 +36,15 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+// Single Animal Filter
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+// Get Animals 
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -41,6 +52,17 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 })
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+// Listen Port
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
