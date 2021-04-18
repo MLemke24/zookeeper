18260d5203471137367b7053a40130d08ba04ede
Matthew Lemke <matthewlemke@Matthews-MBP.lan>
2021-04-18 18:30:10 -0500

    mod 2 done

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals')
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+  // parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+  // parse incoming JSON data
+app.use(express.json());
+  
 
 // Query Filter
 
@@ -24,7 +31,7 @@ function filterByQuery(query, animalsArray) {
 
     }
     if (query.diet) {
-        filteredResults = filteredResult.filter(animal => animal.diet === query.diet);
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet)
     }
     if (query.species) {
         filteredResults = filteredResults.filter(animal => animal.species === query.species)
@@ -41,6 +48,36 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+  }
+
+// Creat New Animal
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    return animal;
+}
+
+// validate info
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
   }
 
 // Get Animals 
@@ -61,6 +98,19 @@ app.get('/api/animals/:id', (req, res) => {
       res.send(404);
     }
   });
+
+  // Data Stored In Server
+  app.post('/api/animals', (req, res) => {
+      // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    // add animal to json file and animals array in this functino
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+    }
+  })
 
 // Listen Port
 
